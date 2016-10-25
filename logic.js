38e27743a45470f69c69d402df9dd13d76e44136
Architex <b1harrison@scu.edu>
2016-10-25 10:35:23 -0700

    New Logic File
    Created new .js file for page logic, started outlining functions for
    update events.

---
+++ logic.js
@@ -0,0 +1,21 @@
+//File:		logic.js
+//Project:	COEN 174 Degree Tracker
+//By:		Brett Harrison (@Architex)
+//Checkboxes and dropdowns for department.
+//Events are checkbox clicks and dropdown add clicks.
+//Dropdown will contain checkbox info--any classes satisfied by a dropdown
+//	will be checked off. 
+//Mouse-over option to delete nodes
+//
+
+function addClass(){
+
+}
+
+function delClass(){
+
+}
+
+function colorUpdate(){
+	
+}
