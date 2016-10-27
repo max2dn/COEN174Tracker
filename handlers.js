e4d40fcde3401f8191ff09ebfba850af2310df37
Architex <b1harrison@scu.edu>
2016-10-27 11:06:14 -0700

    Major Functionality Update
    HTML:
    -Changed some of the appearence of the dropdowns.
    -Changed checkboxes to characters, all change on click.
    JS:
    -Changed 'logic.js' to 'handlers.js' to better represent the content and
    purpose of file
    -Dropdowns work
    -"Adding" a class places a new list item underneath that dropdown prompt
    -List items will turn red when hovered over
    -List items will be removed when clicked on
    -If a list item already exists, it cannot be added again to either list
    -"Future" list items are denoted by HTML class 'fCourse'

---
+++ handlers.js
@@ -0,0 +1,72 @@
+//File:		handlers.js
+//Project:	COEN 174 Degree Tracker
+//By:		Brett Harrison (@Architex)
+//Checkboxes and dropdowns for department.
+//Events are checkbox clicks and dropdown add clicks.
+//Dropdown will contain checkbox info--any classes satisfied by a dropdown
+//	will be checked off. 
+//Mouse-over option to delete nodes
+//
+
+function checkbox(id){
+	//updateArray(this.value);
+	if(id.style.color == "green")
+		id.style.color = "black";
+	else
+		id.style.color = "green";
+}
+
+function updateArray(item){
+	if(greenClasses.find(item))
+		return;
+
+}
+
+//Start with an empty list, with each click add a list element with the Dept and Number
+function dropdown(id){
+
+	var parent;
+	var text;
+	var node = document.createElement("LI");
+	var nodeId;
+
+	if(id.id == "otherButton"){
+		parent = document.getElementById("otherList");
+		text = document.createTextNode(document.getElementById("otherDropdown").value+' '+document.getElementById("otherNumber").value);
+		nodeId = document.getElementById("otherDropdown").value+' '+document.getElementById("otherNumber").value;
+	}
+	else if(id.id == "futureButton"){
+		parent = document.getElementById("futureList");
+		text = document.createTextNode(document.getElementById("futureDropdown").value+' '+document.getElementById("futureNumber").value);
+		nodeId = document.getElementById("futureDropdown").value+' '+document.getElementById("futureNumber").value;
+		node.setAttribute("class", "fCourse");
+	}
+
+	for(var i = 0; i < document.getElementById("otherList").children.length; i++){
+		if(nodeId == document.getElementById("otherList").children[i].id)
+			return;
+	}
+	for(var j = 0; j < document.getElementById("futureList").children.length; j++){
+		if(nodeId == document.getElementById("futureList").children[j].id)
+			return;
+	}
+	node.appendChild(text);
+	node.id = nodeId;
+	node.setAttribute("onmouseenter", "delHoverOn(this)");
+	node.setAttribute("onmouseleave", "delHoverOff(this)");
+	node.setAttribute("onclick", "clickChild(this)");
+	parent.appendChild(node);
+}
+
+function delHoverOn(id){
+	id.style.color = "red";
+}
+
+function delHoverOff(id){
+	id.style.color = "black";
+}
+
+function clickChild(id){
+	var parent = id.parentElement;
+	parent.removeChild(id);
+}
